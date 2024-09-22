38475ce7fee023ca68e6e069b42516f31a87043f
artudi54 <artudi54@gmail.com>
2024-09-22 15:46:43 +0200

    Refactored config module

--- programs_integrator/config/Config.py
+++ programs_integrator/config/Config.py
@@ -3,33 +3,38 @@ import sys
 import pathlib
 import configparser
 import sortedcontainers
+import distutils.util
 from programs_integrator.config.ApplicationDir import ApplicationDir
 from programs_integrator.config.UserApplicationDir import UserApplicationDir
 
 
 class Config:
-    SECTION_CONFIG = "Config"
-    KEY_APPEND_EXTENSION = "AppendExtension"
+    _SECTION_CONFIG = "Config"
+    _KEY_APPEND_EXTENSION = "AppendExtension"
+    _KEY_UPDATE_DELAY = "UpdateDelay"
+
+    class User:
+        def __init__(self):
+            self.home_path = pathlib.Path.home()
+            self.autostart_path = self.home_path / ".config" / "autostart"
+            self.config_dir = self.home_path / ".config" / "programs-integrator"
+            self.config_file = self.config_dir / "config.ini"
+            self.excluded_file = self.config_dir / "excluded.txt"
 
     def __init__(self):
-        self.home_path = pathlib.Path.home()
-        self.autostart_path = self.home_path / ".config" / "autostart"
-        self.config_dir = self.home_path / ".config" / "desktop-entry-integrator"
-        self.config_file = self.config_dir / "config.ini"
-        self.excluded_file = self.config_dir / "excluded.txt"
-
-        self.application_dirs = Config._read_application_dirs(self.home_path)
+        self.user = Config.User()
+        self.application_dirs = Config._read_application_dirs(self.user.home_path)
 
         self.append_extension = True
         self.update_delay = 10
         self.excluded_desktop_entries = set()
 
-        if not self.config_dir.exists():
-            self.config_dir.mkdir(parents=True, exist_ok=True)
-        if not self.config_file.exists():
-            self.config_file.touch(exist_ok=True)
-        if not self.excluded_file.exists():
-            self.excluded_file.touch(exist_ok=True)
+        if not self.user.config_dir.exists():
+            self.user.config_dir.mkdir(parents=True, exist_ok=True)
+        if not self.user.config_file.exists():
+            self.user.config_file.touch(exist_ok=True)
+        if not self.user.excluded_file.exists():
+            self.user.excluded_file.touch(exist_ok=True)
         self.read_config()
 
     @staticmethod
@@ -52,31 +57,49 @@ class Config:
 
     def read_config(self):
         parser = configparser.ConfigParser()
-        parser.read(self.config_dir)
+        parser.optionxform = str
+        parser.read(self.user.config_file)
 
-        if Config.SECTION_CONFIG in parser:
-            config_section = parser[Config.SECTION_CONFIG]
-            if Config.KEY_APPEND_EXTENSION in config_section:
-                self.append_extension = config_section[Config.KEY_APPEND_EXTENSION]
+        if Config._SECTION_CONFIG in parser:
+            config_section = parser[Config._SECTION_CONFIG]
+            if Config._KEY_APPEND_EXTENSION in config_section:
+                try:
+                    self.append_extension = bool(distutils.util.strtobool(config_section[Config._KEY_APPEND_EXTENSION]))
+                except ValueError:
+                    pass
+            if Config._KEY_UPDATE_DELAY in config_section:
+                try:
+                    self.update_delay = int(config_section[Config._KEY_UPDATE_DELAY])
+                except ValueError:
+                    pass
 
-        self.excluded_desktop_entries = sortedcontainers.SortedSet(filter(None, open(self.excluded_file).read().splitlines()))
+        self.excluded_desktop_entries = sortedcontainers.SortedSet(filter(None, open(self.user.excluded_file).read().splitlines()))
 
     def write_config(self):
         parser = configparser.ConfigParser()
-        parser[Config.SECTION_CONFIG] = {
-            Config.KEY_APPEND_EXTENSION: str(self.append_extension)
+        parser.optionxform = str
+        parser[Config._SECTION_CONFIG] = {
+            Config._KEY_APPEND_EXTENSION: str(self.append_extension),
+            Config._KEY_UPDATE_DELAY: str(self.update_delay)
         }
-        with open(self.config_file, "w") as configfile:
-            parser.write(configfile)
+        with self.user.config_file.open("w") as config_file:
+            parser.write(config_file)
+        with self.user.excluded_file.open("w") as excluded_file:
+            for entry in self.excluded_desktop_entries:
+                excluded_file.write(entry)
+                excluded_file.write("\n")
 
     def update_application_dirs(self):
-
-        self.application_dirs = Config._read_application_dirs(self.home_path)
+        self.application_dirs = Config._read_application_dirs(self.user.home_path)
 
     def print(self, file=sys.stdout):
-        print("Config is:", file=file)
-        print("\t" + Config.KEY_APPEND_EXTENSION + "=" + str(self.append_extension), file=file)
+        print("Config:", file=file)
+        print("\t" + Config._KEY_APPEND_EXTENSION + "=" + str(self.append_extension), file=file)
+        print("\t" + Config._KEY_UPDATE_DELAY + "=" + str(self.update_delay), file=file)
         print("\tApplicationDirs:")
         for application_dir in self.application_dirs:
             print("\t\t" + application_dir.name + ":", str(application_dir.path), file=file)
+        print("\tExcludedDesktopEntries:")
+        for excluded_entry in self.excluded_desktop_entries:
+            print("\t\t" + excluded_entry)
         print(file=file)
